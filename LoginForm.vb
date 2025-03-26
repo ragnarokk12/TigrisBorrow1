264f70fff7cebcc71e2ed302c147ed0b050d2e8b
AgarYT <sagiripaul@gmail.com>
2025-03-26 21:44:28 +0800

    wgg

--- LoginForm.vb
+++ LoginForm.vb
@@ -177,4 +177,7 @@ Public Class LoginForm
         ' Additional logic can be added here if needed.
     End Sub
 
+    Private Sub Guna2Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Guna2Panel1.Paint
+
+    End Sub
 End Class
